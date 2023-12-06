3dbbe46002c5c61b37eff0e966d84047b39ddda8
Chris Fitzgerald <cf2995@rit.edu>
2023-12-05 21:27:03 -0500

    Repo organization.

--- src/networkDeploy/initScript.sh
+++ src/networkDeploy/initScript.sh
@@ -9,6 +9,6 @@ apt update;
 apt upgrade;
 apt install git-all ansible;
 git clone https://github.com/kdn5436/CMMC-Compliance-Project.git;
-ansible-playbook -c local -i 127.0.0.1, -l 127.0.0.1 CMMC-Compliance-Project/src/lvl2/roles/ubuntu2004STIG/tasks/main.yml;
+ansible-playbook -c local -i 127.0.0.1, -l 127.0.0.1 CMMC-Compliance-Project/src/stiglevel2/roles/ubuntu2004STIG/tasks/main.yml;
 rm -r CMMC-Compliance-Project.git;
 apt remove git-all ansible;
