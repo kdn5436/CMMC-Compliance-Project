89021722d4aeb7eb448a81c86efa02c8223e75a0
Chris Fitzgerald <cf2995@rit.edu>
2023-12-07 16:35:09 -0500

    Updating bash init script

--- src/networkDeploy/initScript.sh
+++ src/networkDeploy/initScript.sh
@@ -10,5 +10,5 @@ apt full-upgrade -y;
 apt install -y git-all ansible;
 git clone https://github.com/kdn5436/CMMC-Compliance-Project.git;
 ansible-playbook -c local -i 127.0.0.1, -l 127.0.0.1 CMMC-Compliance-Project/src/stiglevel2/ubuntu2004-stig.yml;
-rm -r CMMC-Compliance-Project.git;
+rm -r CMMC-Compliance-Project;
 apt remove git-all ansible;
