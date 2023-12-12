5cc3c933be2623948e2f9ca931aed6fff048cb20
Chris Fitzgerald <cf2995@rit.edu>
2023-12-12 17:42:12 -0500

    Fixing init scripts

--- src/networkDeploy/initplc.sh
+++ src/networkDeploy/initplc.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # This script downloads all of the needed utilities for the CMMC project and
-# installs them. This script needs to be run as root because there are commands
-# that need elevated privileges.  This is to be added to the Ansible setup for
-# each workstation.
+# installs them. It also goes the extra step to download the plc editor for
+# Chinda's Chickens.  This script needs to be run as root because there are
+# commands that need elevated privileges.  This is to be added to the Ansible
+# setup for each workstation.
 
 
 apt update;
@@ -12,6 +13,12 @@ git clone https://github.com/kdn5436/CMMC-Compliance-Project.git;
 ansible-playbook -c local -i 127.0.0.1, -l 127.0.0.1 CMMC-Compliance-Project/src/stiglevel2/ubuntu2004-stig.yml;
 rm -rf CMMC-Compliance-Project;
 apt remove -y git-all ansible;
-adduser --home /home/<user>;
+adduser --home /home/<user> --shell /bin/bash <user>;
 usermod -p ChangeMe123! <user>;
+mkdir openplc;
+cd openplc;
 wget https://autonomylogic.com/wp-content/uploads/files/OpenPLC%20Editor%20for%20Linux.zip;
+unzip OpenPLC\ Editor\ for\ Linux.zip;
+cd OpenPLC_Editor/;
+./install.sh;
+cd ~/;
