c10a15030140ca7c994858e4a58ad69a37b1f2a8
Chris Fitzgerald <cf2995@rit.edu>
2023-12-12 17:58:43 -0500

    Fixing init scripts

--- src/networkDeploy/initplc.sh
+++ src/networkDeploy/initplc.sh
@@ -8,13 +8,14 @@
 
 apt update;
 apt full-upgrade -y;
+adduser --home /home/<user> --shell /bin/bash --disabled-login <user>;
+usermod -p ChangeMe123! <user>;
+usermod -U <user>;
 apt install -y git-all ansible;
 git clone https://github.com/kdn5436/CMMC-Compliance-Project.git;
 ansible-playbook -c local -i 127.0.0.1, -l 127.0.0.1 CMMC-Compliance-Project/src/stiglevel2/ubuntu2004-stig.yml;
 rm -rf CMMC-Compliance-Project;
 apt remove -y git-all ansible;
-adduser --home /home/<user> --shell /bin/bash <user>;
-usermod -p ChangeMe123! <user>;
 mkdir openplc;
 cd openplc;
 wget https://autonomylogic.com/wp-content/uploads/files/OpenPLC%20Editor%20for%20Linux.zip;
