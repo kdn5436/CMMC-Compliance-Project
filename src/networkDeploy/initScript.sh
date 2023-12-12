cc6843cf049eff85530b4a9733424950f1719dc7
Chris Fitzgerald <cf2995@rit.edu>
2023-12-12 14:24:10 -0500

    Added users to the init script

--- src/networkDeploy/initScript.sh
+++ src/networkDeploy/initScript.sh
@@ -12,3 +12,5 @@ git clone https://github.com/kdn5436/CMMC-Compliance-Project.git;
 ansible-playbook -c local -i 127.0.0.1, -l 127.0.0.1 CMMC-Compliance-Project/src/stiglevel2/ubuntu2004-stig.yml;
 rm -rf CMMC-Compliance-Project;
 apt remove -y git-all ansible;
+adduser --home /home/<user>
+usermod -p ChangeMe123! <user>
