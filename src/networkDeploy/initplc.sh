035bd0e5bc650684356fb02f7a9dda938209fc02
Chris Fitzgerald <cf2995@rit.edu>
2023-12-12 20:43:36 -0500

    Fixing init scripts

--- src/networkDeploy/initplc.sh
+++ src/networkDeploy/initplc.sh
@@ -9,8 +9,9 @@
 
 apt update;
 apt full-upgrade -y;
-useradd -md /home/$1 -s /bin/bash -p $y$j9T$SALT$DErcnYbi/y22DT77NadDBI.DWhFjxLI6C7fVv8ubrO2 -U $1;
+useradd -md /home/$1 -s /bin/bash -U $1;
 usermod -aG users $1;
+echo "$1:$2" | chpasswd;
 apt install -y git-all ansible;
 git clone https://github.com/kdn5436/CMMC-Compliance-Project.git;
 ansible-playbook -c local -i 127.0.0.1, -l 127.0.0.1 CMMC-Compliance-Project/src/stiglevel2/ubuntu2004-stig.yml;
